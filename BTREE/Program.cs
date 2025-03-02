6284f844e88871b147a801202912d35510236f21
AmmarM <ammar.m777.2@gmail.com>
2025-03-03 00:25:47 +0200

    review binaryTree implementation

---
+++ BTREE/Program.cs
@@ -0,0 +1,20 @@
+﻿ namespace BTREE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BinaryTree<char> tree = new BinaryTree<char>();
+            tree.Insert('A');
+            tree.Insert('B');
+            tree.Insert('C');
+            tree.Insert('D');
+            tree.Insert('E');
+            tree.Insert('F');
+            tree.Insert('G');
+            tree.Insert('H');
+            tree.Insert('I');
+            tree.Print();
+        }
+    }
+}
