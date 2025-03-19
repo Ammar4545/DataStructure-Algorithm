388ccad47b3d55fb19c8b4a92e6e81fbfa4f0352
AmmarM <ammar.m777.2@gmail.com>
2025-03-19 23:40:13 +0200

    setup head DS

---
+++ Heap/Heap.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heap
+{
+    public class Heap
+    {
+        private List<int> _list = new List<int>();
+        private int _size;
+        public Heap()
+        {
+            
+            
+            _list = new List<int>();
+           _size = 0;
+        }
+
+        public void Insert(int data)
+        {
+            int i = _size;
+
+            // Ensure list has enough capacity
+            if (i >= _list.Count)
+            {
+                _list.Add(data);
+            }
+            else
+            {
+                _list[i] = data;
+            }
+            ++_size;
+            _list[i] = data;
+
+            int parent_index = (int)Math.Floor((i - 1) / 2.0);
+
+            while (i != 0 && _list[i] < _list[parent_index])
+            {
+                var temp = _list[i];
+                _list[i] = _list[parent_index];
+                _list[parent_index] = temp;
+
+                i = parent_index;
+                parent_index = (int)Math.Floor((i - 1) / 2.0);
+            }
+        }
+        public void Print()
+        {
+            string printData = "";
+            for (int i = 0; i < _size; i++)
+            {
+                printData += _list[i] + " - ";
+            }
+            Console.WriteLine(printData);
+        }
+
+        public int Size()
+        {
+            return _size;
+        }
+
+        public void Draw()
+        {
+            int levelsCount = (int)Math.Log(_size, 2) + 1;
+            int lineWidth = (int)Math.Pow(2, levelsCount - 1);
+
+            int j = 0;
+            for (int i = 0; i < levelsCount; i++)
+            {
+                int nodesCount = (int)Math.Pow(2, i);
+                int space = (int)Math.Ceiling(lineWidth - nodesCount / 2.0);
+                int spaceBetween = (int)Math.Ceiling(levelsCount / (double)nodesCount);
+                spaceBetween = spaceBetween < 1 ? 1 : spaceBetween;
+                int k = j;
+                string str = new string(' ', space + spaceBetween);
+
+                for (; j < k + nodesCount; j++)
+                {
+                    if (j == _size)
+                    {
+                        break;
+                    }
+
+                    str += _list[j] + new string(' ', spaceBetween);
+                }
+
+                str += new string(' ', space) + "\n";
+                Console.WriteLine(str);
+            }
+        }
+    }
+}
---
+++ Heap/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Heap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var heap = new Heap();
+            heap.Insert(14);
+            heap.Insert(16);
+            heap.Insert(32);
+            heap.Insert(45);
+            heap.Insert(53);
+            heap.Insert(20);
+            heap.Insert(24);
+            heap.Insert(27);
+            heap.Insert(15);
+
+            heap.Print();
+            heap.Draw();
+        }
+    }
+}
